3616f7aeae8542ed90b729b234a36b62c41277c4
Steven Masley <stevenmasley@gmail.com>
2025-01-26 12:38:24 -0600

    init

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "Development environment";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+          config = {
+            permittedInsecurePackages = [
+            ];
+          };
+        };
+        formatter = pkgs.nixpkgs-fmt;
+        # Check in https://search.nixos.org/packages to find new packages.
+        # Use `nix --extra-experimental-features nix-command --extra-experimental-features flakes flake update`
+        # to update the lock file if packages are out-of-date.
+
+        # From https://nixos.wiki/wiki/Google_Cloud_SDK
+
+
+        devShellPackages = with pkgs; [
+          antlr4_12
+        ];
+      in
+      {
+        defaultPackage = formatter; # or replace it with your desired default package.
+        devShell = pkgs.mkShell { buildInputs = devShellPackages; };
+      }
+    );
+}
---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/Emyrk/sqlsearch
+
+go 1.23.4
+
+require (
+	github.com/pganalyze/pg_query_go/v6 v6.0.0 // indirect
+	google.golang.org/protobuf v1.31.0 // indirect
+)
